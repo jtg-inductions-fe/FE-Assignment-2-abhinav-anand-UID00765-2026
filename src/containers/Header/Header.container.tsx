2e190d2a209f46b6d6b331d18e5c3f28a899e367
Abhinav Anand <abhinav.anand@joshtechnologygroup.com>
2026-09-05 16:24:29 +0530

    [AA_A2_03]: Completed Navbar (#5)

--- src/containers/Header/Header.container.tsx
+++ src/containers/Header/Header.container.tsx
@@ -1,20 +1,108 @@
-import { Toolbar, Typography } from '@mui/material';
+import { useState } from 'react';
 
-import { ErrorBoundary } from '@components';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import { Button, IconButton, Menu, Typography } from '@mui/material';
+
+import { Avatar, ErrorBoundary } from '@components';
 import { PATHS } from '@constants';
+import { logout, RootState } from '@store';
 
-import { StyledBrandLink, StyledNavbar } from './Header.styles';
+import {
+    StyledBrandLink,
+    StyledMenuItem,
+    StyledNavbar,
+    StyledToolbar,
+} from './Header.styles';
 
-export const Header = () => (
-    <ErrorBoundary>
+export const Header = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const { user } = useSelector((state: RootState) => state.auth);
+
+    // For defining the anchor element of the menu list, when opened
+    const [anchorElement, setAnchorElement] = useState<null | HTMLElement>(
+        null,
+    );
+
+    // If the Menu is currently open
+    const open = Boolean(anchorElement);
+
+    // To open the menu list
+    const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElement(event.currentTarget);
+    };
+
+    // To close the menu list
+    const handleMenuClose = () => {
+        setAnchorElement(null);
+    };
+
+    // For logging out from
+    const handleLogout = () => {
+        handleMenuClose();
+        dispatch(logout());
+        void navigate(PATHS.LOGIN);
+    };
+
+    return (
         <StyledNavbar position="sticky">
-            <Toolbar>
-                <Typography variant="h6" component="h1">
-                    <StyledBrandLink to={PATHS.HOME}>
-                        GitHub Explorer
-                    </StyledBrandLink>
-                </Typography>
-            </Toolbar>
+            <ErrorBoundary>
+                <StyledToolbar>
+                    <Typography variant="h6" component="h1">
+                        <StyledBrandLink to={PATHS.HOME}>
+                            GitHub Explorer
+                        </StyledBrandLink>
+                    </Typography>
+
+                    {!user ? (
+                        <Button
+                            variant="contained"
+                            onClick={() => void navigate(PATHS.LOGIN)}
+                        >
+                            Login
+                        </Button>
+                    ) : (
+                        <>
+                            <IconButton onClick={handleMenuOpen}>
+                                <Avatar
+                                    alt={user.login}
+                                    src={user.avatar_url}
+                                    avatarSize="md"
+                                />
+                            </IconButton>
+
+                            <Menu
+                                anchorEl={anchorElement}
+                                open={open}
+                                onClose={handleMenuClose}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'right',
+                                }}
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                            >
+                                <StyledMenuItem
+                                    onClick={() => {
+                                        handleMenuClose();
+                                        void navigate(PATHS.PROFILE);
+                                    }}
+                                >
+                                    Profile
+                                </StyledMenuItem>
+                                <StyledMenuItem onClick={handleLogout}>
+                                    Logout
+                                </StyledMenuItem>
+                            </Menu>
+                        </>
+                    )}
+                </StyledToolbar>
+            </ErrorBoundary>
         </StyledNavbar>
-    </ErrorBoundary>
-);
+    );
+};
